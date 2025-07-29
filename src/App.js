37c030f22a9d644cdcf27a05591b6ab9035932e3
Aaron-GF <aarongarcia567@gmail.com>
2025-07-29 15:36:00 +0200

    finaliza ejecicio creando rutas con react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Root from './components/root';
 
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-// create router with JSX Route elements
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Root />}>
       <Route index element={<HomePage />} />
       <Route path=':type' element={<HomePage />} />
+      <Route path=':type/:id' element={<PetDetailsPage/>} />
+      <Route path='/search' element={<SearchPage />} />
+      <Route path='/pet-details-not-found' element={<PetDetailsNotFound />} />
     </Route>
   ));
 
